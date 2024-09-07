c82a3d65a559f7012887c8b44674c5e00897b84b
RomainDoyen <romain.doyen97@gmail.com>
2024-09-07 10:44:11 +0400

    feat: Route configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,14 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 export function App() {
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/likes" element={<p>Likes</p>} />
+        <Route path="/movie/:id" element={<p>Movie</p>} />
+        <Route path="*" element={<p>Error</p>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
