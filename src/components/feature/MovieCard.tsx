f0ce4496978d233dddbd9640f28e2c43b6e8e3b0
RomainDoyen <romain.doyen97@gmail.com>
2024-09-08 17:22:49 +0400

    ci: Use an object to store genres rather than a switch

--- src/components/feature/MovieCard.tsx
+++ src/components/feature/MovieCard.tsx
@@ -3,81 +3,21 @@ import { MovieCardProps } from '../../types/movie';
 import { dateFormater, addStorage, deleteStorage } from '../../utils/moviecrd';
 import Button from '../ui/Button';
 import Image from '../ui/Image';
+import { genres } from '../../utils/movieGenre';
 
 export default function MovieCard ({ movie }: MovieCardProps): React.JSX.Element {
 
     const apiImage: string = "https://image.tmdb.org/t/p/original";
 
     const genreFinder = () => {
-        let genreArray: string[] = [];
-        for (let index = 0; index < movie.genre_ids.length; index++) {
-            switch (movie.genre_ids[index]) {
-                case 28:
-                    genreArray.push(`Action`);
-                    break;
-                case 12:
-                    genreArray.push(`Aventure`);
-                    break;
-                case 16:
-                    genreArray.push(`Animation`);
-                    break;
-                case 35:
-                    genreArray.push(`Comédie`);
-                    break;
-                case 80:
-                    genreArray.push(`Policier`);
-                    break;
-                case 99:
-                    genreArray.push(`Documentaire`);
-                    break;
-                case 18:
-                    genreArray.push(`Drame`);
-                    break;
-                case 10751:
-                    genreArray.push(`Famille`);
-                    break;
-                case 14:
-                    genreArray.push(`Fantasy`);
-                    break;
-                case 36:
-                    genreArray.push(`Histoire`);
-                    break;
-                case 27:
-                    genreArray.push(`Horreur`);
-                    break;
-                case 10402:
-                    genreArray.push(`Musique`);
-                    break;
-                case 9648:
-                    genreArray.push(`Mystère`);
-                    break;
-                case 10749:
-                    genreArray.push(`Romance`);
-                    break;
-                case 878:
-                    genreArray.push(`Science-fiction`);
-                    break;
-                case 10770:
-                    genreArray.push(`Téléfilm`);
-                    break;
-                case 53:
-                    genreArray.push(`Thriller`);
-                    break;
-                case 10752:
-                    genreArray.push(`Guerre`);
-                    break;
-                case 37:
-                    genreArray.push(`Western`);
-                    break;
-                default:
-                    break;
-            }
-        }
-        return genreArray.map((genre: string, index: number) => 
-            <li key={index}>
-                {genre}
-            </li>
-        );
+        return movie.genre_ids
+            .map((id: number) => genres[id])
+            .filter(Boolean) // Filtre les valeurs undefined au cas où un genre ID n'est pas trouvé
+            .map((genre: string, index: number) => 
+                <li key={index}>
+                    {genre}
+                </li>
+            );
     }
 
     return (
---
+++ src/utils/movieGenre.ts
@@ -0,0 +1,21 @@
+export const genres: { [key: number]: string } = {
+  28: "Action",
+  12: "Aventure",
+  16: "Animation",
+  35: "Comédie",
+  80: "Policier",
+  99: "Documentaire",
+  18: "Drame",
+  10751: "Famille",
+  14: "Fantasy",
+  36: "Histoire",
+  27: "Horreur",
+  10402: "Musique",
+  9648: "Mystère",
+  10749: "Romance",
+  878: "Science-fiction",
+  10770: "Téléfilm",
+  53: "Thriller",
+  10752: "Guerre",
+  37: "Western"
+};
